96e21d62cb2fd87baf09b0234d60352f3bab271a
johnyworld <johnyworld@naver.com>
2022-03-16 16:09:38 +0900

    Fix routes bug

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,21 @@
-import React from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import routes, { getWashedPathname } from '../../routes';
 import './Header.scss';
 
 const Header: React.FC = () => {
   const { pathname } = useLocation();
   const path = getWashedPathname(pathname).split('/')[1];
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    navigate(routes.root);
+    // eslint-disable-next-line
+  }, []);
+
   return (
     <header className='header'>
-      <Link className={path === '' ? 'selected' : ''} to={routes.root}>
+      <Link className={!path ? 'selected' : ''} to={routes.root}>
         List
       </Link>
       <Link className={path === 'grid' ? 'selected' : ''} to={routes.grid}>
